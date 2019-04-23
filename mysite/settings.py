b8699499edd9f1aa6e1da66d008634d14e252823
amit  desktop <amit@gmail.com>
2019-04-23 11:08:45 +0530

    final code without html/css

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'task',
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- task/forms.py
+++ task/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Person, State, City
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 class PersonForm(forms.ModelForm):
     class Meta:
